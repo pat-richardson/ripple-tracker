62031f4f64402a0cc5c1b080ea9e51e380a04b72
Patrick Richardson <prichardson@conversantmedia.com>
2017-07-14 13:43:04 -0500

    add tweaks from paul to run in background when out of focus

--- app.js
+++ app.js
@@ -1,73 +1,89 @@
 (function() {
 
-    var config = {
-    currencies: ['BTC', 'ETH', 'XRP'],
-    current_rate: {},
-    last_updated: null
-    };
+ var config = {
+  baseUrl: 'https://coincap.io/sscoins',
+  currencies: ['BTC', 'ETH', 'XRP'],
+  current_rate: {},
+  last_updated: null
+};
 
-    function timeNow() {
-    var d = new Date(),
-        h = (d.getHours()<10?'0':'') + d.getHours(),
-        m = (d.getMinutes()<10?'0':'') + d.getMinutes();
-    config.last_updated = h + ':' + m;
-    console.log('updating time...' + config.last_updated);
-    $('#update_ts').text(config.last_updated);
-    }
+var lastUpdated, updateRate = 5000;
 
-    function updateBoard() {
-    console.log('updateBoard');
-    var list = $('<ul/>');
-    for(var i = 0; i < config.currencies.length; i++) {
-        var key = config.currencies[i];    
-        var listItem = $('<li/>');
-        var textItem = $('<p>')
-            .text(config.current_rate[key].name + ': $' + config.current_rate[key].price + ' ('+config.current_rate[key].trend +'%)')
-            .appendTo(listItem);
-        $(listItem).appendTo(list);
-    }
-    $('.market-watch-board').html(list);
-    }
+function timeNow() {
+  var d = new Date(),
+      h = (d.getHours()<10?'0':'') + d.getHours(),
+      m = (d.getMinutes()<10?'0':'') + d.getMinutes(),
+      s = (d.getSeconds()<10?'0':'') + d.getSeconds();
+  config.last_updated = h + ':' + m + ':' + s;
+  console.log('updating time...' + config.last_updated);
+  $('#update_ts').text(config.last_updated);
+}
 
-    function log() {
-    updateBoard();
-    }
+function updateBoard() {
+  console.log('updateBoard');
+  var list = $('<ul/>');
+  for(var i = 0; i < config.currencies.length; i++) {
+    var key = config.currencies[i];    
+    var listItem = $('<li/>');
+    var textItem = $('<p>')
+          .text(config.current_rate[key].name + ': $' + config.current_rate[key].price + ' ('+config.current_rate[key].trend +'%)')
+          .appendTo(listItem);
+    $(listItem).appendTo(list);
+  }
+  $('.market-watch-board').html(list);
+}
 
-    function createRate(values) {
-    return {
-        name: values.long,
-        price: values.price,
-        trend: values.cap24hrChange
-    };
-    }
+function log() {
+  updateBoard();
+}
 
-    function showResults(filteredData) {
-    for(var i = 0; i < filteredData.length; i++) {
-        var crypto = filteredData[i].short;
-        config.current_rate[crypto] = createRate(filteredData[i]);
-    }
-    timeNow();
-    log();
-    }
+function createRate(values) {
+  return {
+    name: values.long,
+    price: values.price,
+    trend: values.cap24hrChange
+  };
+}
 
-    function filterResults(data) {
-    var results = data.filter(function(item) {
-        return config.currencies.indexOf(item.short.toUpperCase()) > -1;
-    });
-    showResults(results);
-    }
+function showResults(filteredData) {
+  for(var i = 0; i < filteredData.length; i++) {
+    var crypto = filteredData[i].short;
+    config.current_rate[crypto] = createRate(filteredData[i]);
+  }
+  timeNow();
+  log();
+}
 
-    function fetchLatestRates() {
-    console.log('updating...');
-    $.get('https://coincap.io/sscoins')
-    .done(filterResults);
-    }
+function filterResults(data) {
+  var results = data.filter(function(item) {
+    return config.currencies.indexOf(item.short.toUpperCase()) > -1;
+  });
+  showResults(results);
+}
 
-    function init() {
-    fetchLatestRates();
-    setTimeout(fetchLatestRates, 60000);
-    }
+function fetchLatestRates() {
+  var elapsedTime = Date.now() - lastUpdated;
+  var playback = elapsedTime / updateRate;
+  if(playback < 0 || playback > 1) {
+    $.get(config.baseUrl)
+      .done(filterResults);
+    update();
+  }
+  else {
+    requestAnimationFrame(fetchLatestRates);    
+  }
+}
 
-    init();
+function init() {
+  $.get(config.baseUrl)
+      .done(filterResults);
+  update()
+}
 
+function update() {
+  lastUpdated = Date.now()
+  requestAnimationFrame(fetchLatestRates)
+}
+
+init()
 })();
