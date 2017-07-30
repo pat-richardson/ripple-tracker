09839c76bcc9fd8e2d53035c38f8d4a0a480bd28
Patrick Richardson <prichardson@conversantmedia.com>
2017-07-30 11:19:31 -0500

    add ability to enter ripple wallet address

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 //add new currencies below
  var config = {
   baseUrl: 'https://coincap.io/sscoins',
+  ripple_url: 'https://data.ripple.com/v2/accounts/PLACEHOLDER/transactions?&limit=15&descending=true',
   currencies: ['BTC', 'ETH', 'XRP'],
   current_rate: {},
-  last_updated: null
+  last_updated: null,
+  wallet: {
+    address: null,
+    amount: 0,
+    value: 0
+  }
 };
 
 var lastUpdated, updateRate = 5000;
@@ -15,12 +21,10 @@ function timeNow() {
       m = (d.getMinutes()<10?'0':'') + d.getMinutes(),
       s = (d.getSeconds()<10?'0':'') + d.getSeconds();
   config.last_updated = h + ':' + m + ':' + s;
-  console.log('updating time...' + config.last_updated);
   $('#update_ts').text(config.last_updated);
 }
 
 function updateBoard() {
-  console.log('updateBoard');
   var list = $('<ul/>');
   for(var i = 0; i < config.currencies.length; i++) {
     var key = config.currencies[i];    
@@ -31,6 +35,7 @@ function updateBoard() {
     $(listItem).appendTo(list);
   }
   $('.market-watch-board').html(list);
+  updateRippleNetworth();
 }
 
 function log() {
@@ -75,6 +80,8 @@ function fetchLatestRates() {
 }
 
 function init() {
+  document.querySelector('#wallet-btn').addEventListener('click', setWalletAddress);
+  document.querySelector('#hide-btn').addEventListener('click', hideControls);
   $.get(config.baseUrl)
       .done(filterResults);
   update()
@@ -85,5 +92,71 @@ function update() {
   requestAnimationFrame(fetchLatestRates)
 }
 
-init()
+/**
+ * Code for updating wallet
+ */
+
+function add(a,b) {
+  return parseInt(a) + parseInt(b);
+}
+
+function formatZerps(total) {
+  return parseFloat(total) / 1e6;
+}
+
+function showZerpsStats() {
+  $('.hide-element').toggleClass();
+}
+
+function hideControls() {
+  $('.wallet-controls').toggleClass('hide-element');
+}
+
+function extractTransactionsFromWalletResponse(data) {
+  var count = data.count;
+  var zerpTotal = data.transactions.map(function(el,index,array) {
+    if(el.hasOwnProperty('tx')) {
+          return el.tx.Amount; 
+    }
+  }).filter(function(el,i,a) {
+    return el !== undefined;
+  }).reduce(add);
+  config.wallet.amount = formatZerps(zerpTotal); 
+  showZerpsStats();
+}
+
+function getWalletData() {
+  if(config.wallet.address !== null) {
+    var url = config.ripple_url.replace('PLACEHOLDER', config.wallet.address);
+    $.get(url)
+      .done(extractTransactionsFromWalletResponse);
+  }
+}
+
+function setWalletAddress() {
+  var userAddress = $('#wallet-address').val();
+  if(userAddress !== '') {
+    $('#wallet-address').val('');
+    config.wallet.address = userAddress;
+    getWalletData();
+  } else {
+    alert('please enter a valid address');
+  }
+}
+
+function updateRippleNetworth() {
+  if(config.wallet.address !== null) {
+    config.wallet.value = '$' + parseFloat((config.current_rate.XRP.price * config.wallet.amount).toFixed(2));
+    $('#xrp_total').text(config.wallet.amount);
+    $('#xrp_value').text(config.wallet.value);
+  }
+}
+
+/**
+ * End code for updating wallet
+ */
+
+$(function() {
+  init()
+});
 })();
